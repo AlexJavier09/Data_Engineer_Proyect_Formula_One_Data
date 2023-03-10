965118edfb248123d697d762598cee7f6e8d4b08
AlexJavier09 <AlexJavier09>
2023-03-10 04:53:17 +0000

    se sube del proyecto

---
+++ Formula 1 (1)/formula1/includes/configuration.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/f1dlalexproject/raw"
+processed_folder_path  ="/mnt/f1dlalexproject/processed"
+presentation_folder_path  ="/mnt/f1dlalexproject/presentation"
+
+# COMMAND ----------
+
+
